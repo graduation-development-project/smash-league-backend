41bfcc63c9af20bf3bc3e91f1eabeb95f2655022
Vinh Son <sonpv07@gmail.com>
2025-02-21 01:35:42 +0700

    fix checking role

--- src/infrastructure/guards/auth/role.guard.ts
+++ src/infrastructure/guards/auth/role.guard.ts
@@ -7,19 +7,18 @@ import { Role } from "../../enums/role.enum";
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-	constructor(private readonly reflector: Reflector) {
-	}
+	constructor(private readonly reflector: Reflector) {}
 
 	canActivate(
 		context: ExecutionContext,
 	): boolean | Promise<boolean> | Observable<boolean> {
-		const roles: string[] = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
-			context.getHandler(),
-			context.getClass(),
-		]);
+		const roles: string[] = this.reflector.getAllAndOverride<Role[]>(
+			ROLES_KEY,
+			[context.getHandler(), context.getClass()],
+		);
 		const { user }: IRequestUser = context.switchToHttp().getRequest();
 
 		// @ts-ignore
-		return user?.userRoles.some((userRole: string) => roles.includes(parseInt(userRole)));
+		return user?.userRoles.some((userRole: string) => roles.includes(userRole));
 	}
 }
