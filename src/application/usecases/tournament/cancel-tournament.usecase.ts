34447dc6aaed37bbd53f9d2b1dac59f7f427dbdb
Vinh Son <sonpv07@gmail.com>
2025-04-22 00:31:58 +0700

    send notification after cancel tournament

--- src/application/usecases/tournament/cancel-tournament.usecase.ts
+++ src/application/usecases/tournament/cancel-tournament.usecase.ts
@@ -1,11 +1,11 @@
 import { ApiResponse } from "../../../domain/dtos/api-response";
 import { BadRequestException, HttpStatus, Inject } from "@nestjs/common";
 import { TournamentRepositoryPort } from "../../../domain/repositories/tournament.repository.port";
-import { TournamentEventStatus } from "@prisma/client";
 import { getRegistrationFee } from "../../../infrastructure/util/get-registration-fee.util";
 import { PaybackFeeListRepositoryPort } from "../../../domain/repositories/payback-fee-list.repository.port";
-import { TournamentEventRepositoryPort } from "../../../domain/repositories/tournament-event.repository.port";
 import { PrismaService } from "../../../infrastructure/services/prisma.service";
+import { NotificationsRepositoryPort } from "../../../domain/repositories/notifications.repository.port";
+import { CreateNotificationDTO } from "../../../domain/dtos/notifications/create-notification.dto";
 
 export class CancelTournamentUseCase {
 	constructor(
@@ -14,8 +14,8 @@ export class CancelTournamentUseCase {
 		private tournamentRepository: TournamentRepositoryPort,
 		@Inject("PaybackFeeListRepositoryPort")
 		private paybackFeeListRepository: PaybackFeeListRepositoryPort,
-		@Inject("TournamentEventRepository")
-		private tournamentEventRepositoryPort: TournamentEventRepositoryPort,
+		@Inject("NotificationRepository")
+		private notificationRepository: NotificationsRepositoryPort,
 	) {}
 
 	async execute(tournamentId: string): Promise<ApiResponse<null>> {
@@ -54,6 +54,15 @@ export class CancelTournamentUseCase {
 			if (paybackRecords.length > 0) {
 				await this.paybackFeeListRepository.createManyPaybackFee(
 					paybackRecords,
+				);
+
+				const notificationData: CreateNotificationDTO = {
+					title: "The event you participate in is canceled",
+					message: `The event  ${event.tournamentEvent} from tournament ${tournament.name} has been canceled. You registration fee will be refunded soon`,
+				};
+				await this.notificationRepository.createNotification(
+					notificationData,
+					paybackRecords.map((user) => user.userId),
 				);
 			}
 		}
