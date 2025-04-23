01642a5ca7745baca1e28af37673810e4b22ff9f
alannguyen2003 <nguyenho30112003@gmail.com>
2025-04-23 16:46:48 +0700

    fix bank seeding

---
+++ src/application/usecases/bank/check-bank-account-exist.usecase.ts
@@ -0,0 +1,11 @@
+import { ApiResponse } from "src/domain/dtos/api-response";
+
+export class CheckBankAccountExistUseCase {
+	constructor(
+	){
+	}
+
+	async execute(): Promise<ApiResponse<any>> {
+		return;
+	}
+}
