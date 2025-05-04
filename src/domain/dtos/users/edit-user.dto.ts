ed937d8a38fc4ce97c637e1b8fdc5d49dfca893f
Vinh Son <sonpv07@gmail.com>
2025-05-04 16:57:27 +0700

    fix: add description for user

--- src/domain/dtos/users/edit-user.dto.ts
+++ src/domain/dtos/users/edit-user.dto.ts
@@ -1,11 +1,25 @@
 import { PartialType } from "@nestjs/mapped-types";
 import { CreateUserDTO } from "./create-user.dto";
-import { IsOptional, IsString } from "class-validator";
+import { IsEnum, IsNumber, IsOptional, IsString } from "class-validator";
+import { UserEntity } from "../../entities/authentication/user.entity";
+import { Hands } from "@prisma/client";
 
-export class EditUserDTO extends PartialType(CreateUserDTO) {
+export class EditUserDTO extends PartialType(UserEntity) {
 	@IsOptional()
 	@IsString()
 	location: string;
+
+	@IsOptional()
+	@IsEnum(Hands)
+	hand: Hands;
+
+	@IsOptional()
+	@IsNumber()
+	height: number;
+
+	@IsOptional()
+	@IsString()
+	description: string;
 
 	@IsOptional()
 	@IsString()
