983f3b5e3983a9c09e84c51698cc60afbffa6da8
alannguyen2003 <nguyenho30112003@gmail.com>
2025-05-05 13:43:45 +0700

    add api get all report for staffs

--- src/application/usecases/tournament/report/get-all-report.usecase.ts
+++ src/application/usecases/tournament/report/get-all-report.usecase.ts
@@ -1,5 +1,6 @@
-import { Inject } from "@nestjs/common";
+import { HttpStatus, Inject } from "@nestjs/common";
 import { ApiResponse } from "src/domain/dtos/api-response";
+import { IReportResponse } from "src/domain/dtos/report/report.interface";
 import { ReportRepositoryPort } from "src/domain/repositories/report.repository.port";
 
 export class GetAllReportUseCase {
@@ -9,7 +10,17 @@ export class GetAllReportUseCase {
 	) {
 	}
 
-	async execute(): Promise<ApiResponse<any>> {
-		return;
+	async execute(): Promise<ApiResponse<IReportResponse[]>> {
+		var reports: IReportResponse[] = await this.reportRepository.getAllReport();
+		if (reports.length === 0) return new ApiResponse<null | undefined>(
+			HttpStatus.OK,
+			"No reports found!",
+			null
+		);
+		return new ApiResponse<IReportResponse[]>(
+			HttpStatus.OK,
+			"Get all reports success!",
+			reports
+		);
 	}
 }
--- src/infrastructure/controllers/report.controller.ts
+++ src/infrastructure/controllers/report.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Req, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Post, Req, UseGuards } from "@nestjs/common";
 import { CreateReportUseCase } from "src/application/usecases/tournament/report/create-report.usecase";
 import { JwtAccessTokenGuard } from "../guards/auth/jwt-access-token.guard";
 import { RolesGuard } from "../guards/auth/role.guard";
@@ -8,11 +8,14 @@ import { CreateReport } from "src/domain/dtos/report/report.validation";
 import { UserReport } from "@prisma/client";
 import { IRequestUser } from "src/domain/interfaces/interfaces";
 import { ApiResponse } from "src/domain/dtos/api-response";
+import { GetAllReportUseCase } from "src/application/usecases/tournament/report/get-all-report.usecase";
+import { IReportResponse } from "src/domain/dtos/report/report.interface";
 
 @Controller("/report")
 export class ReportController {
 	constructor(
-		private readonly createReportUseCase: CreateReportUseCase
+		private readonly createReportUseCase: CreateReportUseCase,
+		private readonly getAllReportUseCase: GetAllReportUseCase
 	){
 	}
 
@@ -23,5 +26,12 @@ export class ReportController {
 		@Req() request: IRequestUser,
 		@Body() createReport: CreateReport): Promise<ApiResponse<UserReport>> {
 		return await this.createReportUseCase.execute(request, createReport);
+	}
+
+	@Get("/get-all-reports")
+	@UseGuards(JwtAccessTokenGuard, RolesGuard)
+	@Roles(RoleMap.Staff.name)
+	async getAllReports(): Promise<ApiResponse<IReportResponse[]>> {
+		return await this.getAllReportUseCase.execute()
 	}
 }
