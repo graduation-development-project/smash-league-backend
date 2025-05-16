4bc3ba564236e79ab643d31490ba94c034bff227
alannguyen2003 <nguyenho30112003@gmail.com>
2025-05-16 14:23:59 +0700

    add title for api getting umpire degrees

--- src/domain/dtos/umpire/umpire-degree.interface.ts
+++ src/domain/dtos/umpire/umpire-degree.interface.ts
@@ -4,6 +4,7 @@ import { IUserResponse } from "src/domain/interfaces/user/user.interface";
 export interface UmpireDegreeResponse {
 	id: string;
 	typeOfDegree: TypeOfUmpireDegree;
+	degreeTitle: string;
 	degree: string[];
 	description?: string;
 	user: IUserResponse;
