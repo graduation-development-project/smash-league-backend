4f3a170e6f40ff74e407963cd7721d23bc4577a2
alannguyen2003 <nguyenho30112003@gmail.com>
2025-06-02 19:51:15 +0700

    add seeding registration for umpires

--- prisma/registration-seed.ts
+++ prisma/registration-seed.ts
@@ -5,47 +5,7 @@ import * as bcrypt from "bcryptjs";
 const prisma = new PrismaClient();
 
 async function main() {
-	// const tournamentRegistration: Prisma.TournamentRegistrationCreateManyInput[] = [
-	// 	{
-
-	// 	}
-	// ];
-	const hcmcOpenTournament = await prisma.tournament.findUnique({
-		where: {
-			id: "hcmc-open-2025"
-		},
-		select: {
-			name: true,
-			id: true,
-			tournamentEvents: {
-				select: {
-					id: true,
-					requirements: true,
-					tournamentEvent: true
-				}
-			}
-		}
-	});
-	const user = await prisma.user.findMany({
-		where: {
-			gender: Gender.MALE
-		}
-	});
-	const registration: Prisma.TournamentRegistrationCreateManyInput[] = [
-		{
-			userId: user[0].id,
-			tournamentId: hcmcOpenTournament.id,
-			tournamentEventId: hcmcOpenTournament.tournamentEvents[0].id,
-			registrationRole: TournamentRegistrationRole.ATHLETE
-		},
-		{
-			userId: user[1].id,
-			tournamentId: hcmcOpenTournament.id,
-			tournamentEventId: hcmcOpenTournament.tournamentEvents[0].id,
-			registrationRole: TournamentRegistrationRole.ATHLETE
-		}
-	];
-	console.log(hcmcOpenTournament);
+	
 }
 
 
