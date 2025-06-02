23968866019d1f8d409cae0f5e2035d5c7fe0c4b
alannguyen2003 <nguyenho30112003@gmail.com>
2025-06-02 22:39:07 +0700

    add api transaction for organizers

--- prisma/transaction-seed.ts
+++ prisma/transaction-seed.ts
@@ -1,21 +1,57 @@
-import { Prisma, PrismaClient, TeamStatus } from "@prisma/client";
+import { Prisma, PrismaClient, TeamStatus, TransactionStatus, TransactionType } from "@prisma/client";
 import * as bcrypt from "bcryptjs";
 
 // initialize Prisma Client
 const prisma = new PrismaClient();
 
 async function main() {
-	const users = await prisma.user.findMany({
+	await createOrganizerTransaction();
+}
+
+async function createOrganizerTransaction() {
+	const packages = await prisma.package.findMany();
+	const organizers = await prisma.user.findMany({
 		where: {
-			email: {
-				in: [
-					"hoduongtrungnguyen@gmail.com",
-					"nguyenhoangbao@gmail.com"
-				]
+			userRoles: {
+				some: {
+					role: {
+						roleName: "Organizer"
+					}
+				}
 			}
 		}
-	}); 
-	console.log(users);
+	});
+	var transactionsCreate: Prisma.TransactionCreateManyInput[] = [];
+	for (let i = 0; i < organizers.length; i++) {
+		transactionsCreate.push({
+			userId: organizers[i].id,
+			transactionType: TransactionType.BUYING_PAKCKAGE,
+			createdAt: new Date(2024, 4, 31),
+			status: TransactionStatus.SUCCESSFUL,
+			value: packages[0].price,
+			transactionDetail: ""
+		},
+		{
+			userId: organizers[i].id,
+			transactionType: TransactionType.BUYING_PAKCKAGE,
+			createdAt: new Date(2024, 4, 32),
+			status: TransactionStatus.SUCCESSFUL,
+			value: packages[0].price,
+			transactionDetail: ""
+		});
+		await prisma.user.update({
+			where: {
+				id: organizers[i].id
+			},
+			data: {
+				creditsRemain: packages[0].credits * 2
+			}
+		});
+	}
+	console.log(transactionsCreate);
+	const transactionCreated = await prisma.transaction.createManyAndReturn({
+		data:  transactionsCreate
+	});
 }
 
 main()
